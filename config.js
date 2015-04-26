4592a440981d6c832f4b785e739d90e05848b520
Evan Marsland <ERmarsland@gmail.com>
2015-04-25 22:17:01 -0400

    config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = (function(){
 	return {
 		PORT: 3000,
 		yeplive_api: {
-			host: process.env['YEPLIVE_HOST'] || 'http://localhost:8888/yeplive/yeplive-api/public/'
+			host: process.env['YEPLIVE_HOST'] || 'http://development-vriepmhkv2.elasticbeanstalk.com/'
 		}	
 	}
 }());
