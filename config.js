9cd8dfcd9f99b241840a10a8b009420cf80d27a3
Terry Chen <gn02090867@hotmail.com>
2015-05-05 12:15:44 -0400

    g

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = (function(){
 	return {
 		PORT: 3000,
-		host: process.env['HOST'] || 'http://app.yeplive.com',
+		host: process.env['HOST'] || 'http://yeplive-production.elasticbeanstalk.com/',
 		yeplive_api: {
 			host: process.env['YEPLIVE_HOST'] || 'http://yplv.tv/'
 		}	
