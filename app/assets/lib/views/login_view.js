e8a7ef5ba840f518451abb428614610745a2de8d
Terry Chen <gn02090867@hotmail.com>
2015-05-20 15:01:11 -0400

    login modal

--- app/assets/lib/views/login_view.js
+++ app/assets/lib/views/login_view.js
@@ -15,8 +15,10 @@ define(['jquery',
 		},
 		render: function(){
 			this.$el.html(this.tpl());
-			if(true){
-				$('#login-appstore-prompt').modal('show');
+
+			var ua = navigator.userAgent.toLowerCase();
+			if(navigator.appVersion.indexOf("iPad") != -1 || navigator.appVersion.indexOf("iPhone") != -1 || ua.indexOf("android") != -1 || ua.indexOf("ipod") != -1 || ua.indexOf("windows ce") != -1 || ua.indexOf("windows phone") != -1){
+				return $('#login-appstore-prompt').modal('show');
 			}
 		}
 	});
